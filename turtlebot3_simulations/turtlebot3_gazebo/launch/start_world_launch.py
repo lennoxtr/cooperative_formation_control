5db77fd1f136476578268df89e83853e666d48b7
lennoxtr <huykhang.tranp@gmail.com>
2024-12-09 19:08:18 +0800

    Add turtlebot3_simulations

---
+++ turtlebot3_simulations/turtlebot3_gazebo/launch/start_world_launch.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python3
+
+import os
+
+from ament_index_python.packages import get_package_share_directory
+from launch import LaunchDescription
+from launch.actions import DeclareLaunchArgument
+from launch.actions import IncludeLaunchDescription
+from launch.conditions import IfCondition
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch.substitutions import LaunchConfiguration
+from launch_ros.actions import Node
+
+
+def generate_launch_description():
+    pkg_gazebo_ros = get_package_share_directory('gazebo_ros')
+
+    world = os.path.join(
+        get_package_share_directory('turtlebot3_gazebo'),
+        'worlds',
+        'empty_world.world'
+    )
+
+    ## GZ Server
+    gzserver_cmd = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(
+            os.path.join(pkg_gazebo_ros, 'launch', 'gzserver.launch.py')
+        ),
+        launch_arguments={'world': world}.items()
+    )
+
+    ## GZ Client
+    gzclient_cmd = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(
+            os.path.join(pkg_gazebo_ros, 'launch', 'gzclient.launch.py')
+        )
+    )
+
+    ld = LaunchDescription()
+
+    ld.add_action(gzserver_cmd)
+    ld.add_action(gzclient_cmd)
+    
+    return ld
+
+
