f2843747b9571314bdf4ab77390c12f10fd80216
lennoxtr <huykhang.tranp@gmail.com>
2024-12-25 05:20:13 +0700

    Modify heartbeat publisher

--- robot_controller/launch/multiple_robot_controller_launch.py
+++ robot_controller/launch/multiple_robot_controller_launch.py
@@ -5,12 +5,13 @@ def generate_launch_description():
     num_of_robot = 5
     robot_controller_node_list = []
 
-    i = 1
-    while i <= num_of_robot:
+    i = 0
+    while i < num_of_robot:
         node = Node(
             package='robot_controller',
             executable='robot_controller',
             name=f'robot_controller{i}',  # Unique name for each instance
+            parameters=[{'robot_id': i}],
             output='screen',
         )
         robot_controller_node_list.append(node)
