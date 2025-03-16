e73958de7793461c0734f851769a19bf2795710d
lennoxtr <huykhang.tranp@gmail.com>
2025-03-16 21:52:15 +0800

    Test new launch file

--- turtlebot3_simulations/turtlebot3_gazebo/launch/spawn_turtlebot_with_node.py
+++ turtlebot3_simulations/turtlebot3_gazebo/launch/spawn_turtlebot_with_node.py
@@ -1,20 +1,24 @@
 from launch import LaunchDescription
-
-import launch.actions
-import launch_ros.actions
-
+from launch.substitutions import LaunchConfiguration
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch.actions import IncludeLaunchDescription
+import os
+from ament_index_python.packages import get_package_share_directory
 
 def generate_launch_description():
+    pkg_path = get_package_share_directory('turtlebot3_gazebo')
     return LaunchDescription([
-        launch_ros.actions.Node(
-            package='turtlebot3_gazebo', 
-            executable='spawn_turtlebot_launch.py',
-            output='screen',
-            arguments=[
-                '--robot_urdf', launch.substitutions.LaunchConfiguration('robot_urdf'),
-                '--robot_name', launch.substitutions.LaunchConfiguration('robot_name'),
-                '--robot_namespace', launch.substitutions.LaunchConfiguration('robot_namespace'),
-                '-x', launch.substitutions.LaunchConfiguration('x'),
-                '-y', launch.substitutions.LaunchConfiguration('y'),
-                '-z', launch.substitutions.LaunchConfiguration('z')]),
+        IncludeLaunchDescription(
+            PythonLaunchDescriptionSource(
+                os.path.join(pkg_path, 'launch', 'spawn_turtlebot_launch.py')
+            ),
+            launch_arguments={
+                'robot_urdf': LaunchConfiguration('robot_urdf'),
+                'robot_name': LaunchConfiguration('robot_name'),
+                'robot_namespace': LaunchConfiguration('robot_namespace'),
+                'x': LaunchConfiguration('x'),
+                'y': LaunchConfiguration('y'),
+                'z': LaunchConfiguration('z'),
+            }.items(),
+        )
     ])
