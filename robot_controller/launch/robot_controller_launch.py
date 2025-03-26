c52c2c9b59d95140fc40fb13133e72f56743bce1
lennoxtr <huykhang.tranp@gmail.com>
2025-03-27 01:42:19 +0800

    fix bugs in launch file

--- robot_controller/launch/robot_controller_launch.py
+++ robot_controller/launch/robot_controller_launch.py
@@ -1,10 +1,11 @@
-import launch
+
 
 from ament_index_python.packages import get_package_share_directory
 from launch import LaunchDescription
-from launch.actions import DeclareLaunchArgument, IncludeLaunchDescription, PushRosNamespace
+from launch.actions import DeclareLaunchArgument, IncludeLaunchDescription
 from launch.launch_description_sources import PythonLaunchDescriptionSource
 from launch_ros.actions import Node
+from launch_ros.actions import PushRosNamespace
 from launch.substitutions import LaunchConfiguration
 import os
 
