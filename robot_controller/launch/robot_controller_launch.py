900d91dbc374ab4ed31aafebcfbf1d55fbaf0395
lennoxtr <huykhang.tranp@gmail.com>
2025-03-27 14:14:34 +0800

    Remove rviz

--- robot_controller/launch/robot_controller_launch.py
+++ robot_controller/launch/robot_controller_launch.py
@@ -93,7 +93,7 @@ def generate_launch_description():
             package="tf2_ros",
             executable="static_transform_publisher",
             arguments=["0", "0", "0", "0", "0", "0",
-                       , "/base_link",
+                        "/base_link",
                         "/base_scan"],
         ),
     ])
