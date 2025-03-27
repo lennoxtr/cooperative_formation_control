3f22a71dd1a3ad6dfec83ec8561eea1d2f1e0757
lennoxtr <huykhang.tranp@gmail.com>
2025-03-28 01:08:08 +0800

    Dynamic namespace

--- robot_controller/launch/main.launch.py
+++ robot_controller/launch/main.launch.py
@@ -2,12 +2,15 @@ from launch import LaunchDescription
 from launch.actions import DeclareLaunchArgument, IncludeLaunchDescription
 from launch.launch_description_sources import PythonLaunchDescriptionSource
 from launch.substitutions import LaunchConfiguration
+from launch_ros.actions import LifecycleNode
 from ament_index_python.packages import get_package_share_directory
 import os
 
 def generate_launch_description():
     # Declare namespace
     namespace = LaunchConfiguration('namespace', default= os.environ['ROBOT_NAMESPACE'])
+    robot_controller_dir = get_package_share_directory('robot_controller')
+    yaml_map_file = os.path.join(robot_controller_dir, 'maps', 'my_map.yaml')
 
 
     return LaunchDescription([
@@ -28,5 +31,14 @@ def generate_launch_description():
                 os.path.join(get_package_share_directory('robot_controller'), 'launch', 'robot_controller_launch.py')
             ),
             launch_arguments={'namespace': namespace}.items()
+        ),
+
+        LifecycleNode(
+            package='nav2_map_server',
+            executable='map_server',
+            name='map_server',
+            parameters=[{'yaml_filename': yaml_map_file}],
+            namespace=namespace,
+            output='screen',
         ),
     ])
--- robot_controller/launch/robot_controller_launch.py
+++ robot_controller/launch/robot_controller_launch.py
@@ -2,7 +2,7 @@
 
 from ament_index_python.packages import get_package_share_directory
 from launch import LaunchDescription
-from launch.actions import DeclareLaunchArgument, LogInfo
+from launch.actions import DeclareLaunchArgument
 from launch_ros.actions import Node, LifecycleNode
 from launch.substitutions import LaunchConfiguration, PythonExpression
 import os
@@ -12,10 +12,6 @@ def generate_launch_description():
     # Namespace and Robot ID
     namespace = LaunchConfiguration('namespace')
     robot_id_default = PythonExpression(["'", namespace, "'[-1]"])
-
-    # Paths
-    robot_controller_dir = get_package_share_directory('robot_controller')
-    yaml_map_file = os.path.join(robot_controller_dir, 'maps', 'my_map.yaml')
 
     return LaunchDescription([
         # Declare launch arguments
@@ -31,15 +27,6 @@ def generate_launch_description():
             executable='robot_controller',
             name='robot_controller',
             parameters=[{'robot_id': LaunchConfiguration('robot_id')}], 
-            output='screen',
-        ),
-    
-        LifecycleNode(
-            package='nav2_map_server',
-            executable='map_server',
-            name='map_server',
-            parameters=[{'yaml_filename': yaml_map_file}],
-            namespace=namespace,
             output='screen',
         ),
 
