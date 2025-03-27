d03eb6f431ab16e6b38c1392d19d55b57b676605
lennoxtr <huykhang.tranp@gmail.com>
2025-03-27 16:14:14 +0800

    Separate launch files

--- robot_controller/launch/main.launch.py
+++ robot_controller/launch/main.launch.py
@@ -19,6 +19,10 @@ def generate_launch_description():
                 os.path.join(get_package_share_directory('turtlebot3_bringup'), 'launch', 'robot.launch.py')
             ),
             launch_arguments={'namespace': namespace}.items(),  # Ensure namespace consistency
+            remappings=[
+                ('/tf', [namespace, '/tf']),
+                ('/tf_static', [namespace, '/tf_static'])
+            ],
         ),
 
         # Launch additional nodes (LiDAR, AMCL, Map Server, etc.)
