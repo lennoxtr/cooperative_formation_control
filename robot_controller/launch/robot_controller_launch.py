39cfe6397d70f01d1b79ebdc831ea08d5fb48ebf
lennoxtr <huykhang.tranp@gmail.com>
2025-03-30 00:39:46 +0800

    edit rviz

--- robot_controller/launch/robot_controller_launch.py
+++ robot_controller/launch/robot_controller_launch.py
@@ -98,6 +98,7 @@ def generate_launch_description():
             }],
             remappings=[
                 ("/amcl_pose", [namespace, "/amcl_pose"]),
+                ("/scan", "scan"),
                 ("/particlecloud", [namespace, "/particlecloud"]),
                 ("/tf", "tf"),
                 ("/tf_static", "tf_static"),
