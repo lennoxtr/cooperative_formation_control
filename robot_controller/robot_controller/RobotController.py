a2cdbf4605c72d29bccadf25f47aa242c8931ccb
lennoxtr <huykhang.tranp@gmail.com>
2025-04-09 00:30:49 +0800

    test

--- robot_controller/robot_controller/RobotController.py
+++ robot_controller/robot_controller/RobotController.py
@@ -89,7 +89,7 @@ class RobotController(Node):
         
         # Kinematic PID Controller (may add more for different control policies)
         self.PID_position = PidController(Kp=0.0, Ki=0.0, Kd=0.0)
-        self.PID_heading = PidController(Kp=1.0, Ki=0.02, Kd=0.1)
+        self.PID_heading = PidController(Kp=1.0, Ki=0.01, Kd=0.1)
 
         # Pure pursuit settings
         self.lookahead_dist = 0.8
